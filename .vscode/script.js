946b7eaa696e37a55a512fd856b86dfdca137a14
asn <arthuroasn@gmail.com>
2023-08-29 19:00:10 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,9 @@
+function togglemode() {
+  const html = document.documentElement
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+}
