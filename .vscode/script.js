324c5c8bad2be7474a5a1121564ef3ad09350d9b
asn <arthuroasn@gmail.com>
2023-08-31 18:26:24 -0300

    :asn: initial commit

--- .vscode/script.js
+++
@@ -1,9 +0,0 @@
-function togglemode() {
-  const html = document.documentElement
-
-  if (html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  }
-}
